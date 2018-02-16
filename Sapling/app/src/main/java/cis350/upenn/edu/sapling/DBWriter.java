478ebccfe3c04bfbc5b0b063fff29c3e94eb9ad8
Antonio Menarde <amenarde@gmail.com>
2018-02-16 12:57:04 -0500

    removes accidental hw2 from package title and adds method headers for data-level classes

---
+++ Sapling/app/src/main/java/cis350/upenn/edu/sapling/DBWriter.java
@@ -0,0 +1,38 @@
+package cis350.upenn.edu.sapling;
+
+import java.util.List;
+
+/**
+ * Created by amenarde on 2/16/18.
+ */
+
+public class DBWriter {
+    DBWriter (String filePath) {
+        // TODO: Empty method
+    }
+
+    public boolean putDay() {
+        // TODO: Empty method
+        return false;
+    }
+
+    public Day getDay() {
+        // TODO: Empty method
+        return null;
+    }
+
+    public boolean hasDay() {
+        // TODO: Empty method
+        return false;
+    }
+
+    public Day removeDay() {
+        // TODO: Empty method
+        return null;
+    }
+
+    public List<Day> getDayList() {
+        // TODO: Empty method
+        return null;
+    }
+}
---
+++ Sapling/app/src/main/java/cis350/upenn/edu/sapling/Day.java
@@ -0,0 +1,9 @@
+package cis350.upenn.edu.sapling;
+
+/**
+ * Created by amenarde on 2/16/18.
+ */
+
+public class Day {
+
+}
