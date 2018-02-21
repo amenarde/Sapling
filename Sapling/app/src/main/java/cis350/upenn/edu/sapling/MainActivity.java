f0e0995433d35479620c763e545e5103687848f6
makvan <makvan@seas.upenn.edu>
2018-02-21 13:37:31 -0500

    Adds hamburger menu to homepage, fixes display eror
    Hamburger menu, reveals buttons to all other pages; Dispaly, Goal Tracking, Daily Log, Settings

--- Sapling/app/src/main/java/cis350/upenn/edu/sapling/MainActivity.java
+++ Sapling/app/src/main/java/cis350/upenn/edu/sapling/MainActivity.java
@@ -1,18 +1,45 @@
 package cis350.upenn.edu.sapling;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.LayoutInflater.Factory;
 import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_main);
+    }
+    public void startDisplay(View view){
+        Intent i = new Intent(this, DisplayActivity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void startLogging(View view){
+        Intent i = new Intent(this, LoggingActivity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void startGoalTracking(View view){
+        Intent i = new Intent(this, GoalTrackingActivity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void startSettings(View view){
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivityForResult(i, 1);
     }
 
 }
