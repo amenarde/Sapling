2a0e89ff01106511999be9105c0c935d920819e9
juezhou <juezhou@seas.upenn.edu>
2018-02-22 19:53:58 -0500

    Establish logic between data model and model io classes

--- Sapling/app/src/main/java/cis350/upenn/edu/sapling/DataModel.java
+++ Sapling/app/src/main/java/cis350/upenn/edu/sapling/DataModel.java
@@ -1,17 +1,19 @@
 package cis350.upenn.edu.sapling;
 
+import java.io.IOException;
 import java.util.HashSet;
 
 // @author: amenarde
 
-// in memory storage of metrics/goals that are being tracked
+// Singleton class used for in memory storage of metrics/goals
 class DataModel {
     private static DataModel instance;
     static HashSet<String> activeMetrics;
     static HashSet<String> activeGoals;
     static HashSet<String> inactiveMetrics;
     static HashSet<String> inactiveGoals;
-
+    static ModelIO modelIO;    // an IO writer that keeps track of the same data in a local txt file
+    static String modelFilePath = "/path.txt";
 
     static DataModel getinstance() {
         if (instance == null) {
@@ -25,29 +27,35 @@ class DataModel {
         inactiveMetrics = new HashSet<String>();
         activeGoals = new HashSet<String>();
         inactiveGoals = new HashSet<String>();
+        modelIO = new ModelIO(modelFilePath, this);
     }
 
     // add a goal to currently track
-    public void addGoal(String goal) {
+    public void addGoal(String goal) throws IOException {
         activeGoals.add(goal);
+        this.modelIO.updateFile();
     }
     // deprecate a goal
-    public void deprecateGoal(String goal) {
+    public void deprecateGoal(String goal) throws IOException {
         if (activeGoals.contains(goal)) {
             activeGoals.remove(goal);
             inactiveGoals.add(goal);
+            this.modelIO.updateFile();
         }
     }
 
     // add a metric to currently track
-    public void addMetric(String metric) {
+    public void addMetric(String metric) throws IOException {
         activeGoals.add(metric);
+        this.modelIO.updateFile();
     }
+
     // deprecate a metric
-    public void deprecateMetric(String metric) {
+    public void deprecateMetric(String metric) throws IOException {
         if (activeGoals.contains(metric)) {
             activeGoals.remove(metric);
             inactiveGoals.add(metric);
+            this.modelIO.updateFile();
         }
     }
 
