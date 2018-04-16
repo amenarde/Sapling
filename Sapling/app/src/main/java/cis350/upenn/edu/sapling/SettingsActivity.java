0e56f62a26f7a89e5de34aef81bcf7eeb6e4b91e
makvan <makvan@seas.upenn.edu>
2018-04-16 15:32:05 -0400

    Adds functionality to reset buttons in settings

--- Sapling/app/src/main/java/cis350/upenn/edu/sapling/SettingsActivity.java
+++ Sapling/app/src/main/java/cis350/upenn/edu/sapling/SettingsActivity.java
@@ -2,12 +2,23 @@ package cis350.upenn.edu.sapling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Date;
 
 public class SettingsActivity extends AppCompatActivity {
-
+private DataManager dm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        this.dm = DataManager.getInstance();
+    }
+    public void resetAll(View view){
+        dm.purgeFiles(getApplicationContext());
+    }
+    public void resetToday(View view){
+        Date today = new Date();
+        dm.removeDayData(today,getApplicationContext());
     }
 }
