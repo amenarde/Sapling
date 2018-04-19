124682d2ec697fac5951d47bc755b12e05df5852
Tiffany Yue <tyue@seas.upenn.edu>
2018-04-19 16:51:19 -0400

    preps for merge

--- Sapling/app/src/main/java/cis350/upenn/edu/sapling/DisplayActivity.java
+++ Sapling/app/src/main/java/cis350/upenn/edu/sapling/DisplayActivity.java
@@ -39,7 +39,7 @@ public class DisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
-        
+
     }
 
     private void createCheckList() {
